57f72698f72a4144c380f24989558b20bd0dd07f
Saumya-28 <saumya18921@gmail.com>
2023-11-05 15:08:52 +0530

    Building the receipe UI.

--- lib/data/remote/ApiService.dart
+++ lib/data/remote/ApiService.dart
@@ -1,5 +1,13 @@
 import 'package:dio/dio.dart';
 import 'package:fud/models/recipes/Recipe.dart';
+//ApiService defines the end points of the api
+/*An API typically consists of multiple endpoints,
+ each of which serves a specific purpose or provides access to a specific set of data or functionality.
+ These endpoints are used to send requests and receive responses from the API.
+ The endpoint's URL, along with the HTTP method (such as GET, POST, PUT, DELETE, etc.),
+  defines the operation you want to perform on the API.
+  example:nitdgp.org/departments/cse/sectionX/Saumya
+  */
 class ApiService{
   static const String baseurl='https://api.spoonacular.com/';
   static const String endpoint='/recipes/random';
@@ -7,7 +15,12 @@ class ApiService{
   late final Dio dio;
   /*Future is a return type of type Recipe and getrecipes is the name of the function,
   async is used for synchronization, together this whole line defines a function
-  async ensures not to return data until full processing of getting data completes
+  async ensures not to return data until full processing of getting data completes,
+  By marking the function as async and using await,
+  you ensure that your code remains responsive and doesn't block while waiting
+  for the asynchronous operation to complete. The function returns a Future<Recipe>,
+  indicating that it will eventually provide a Recipe object
+  when the asynchronous operation finishes.
   */
   Future<Recipe> getrecipes() async{
     try
--- lib/presentation/home_screens/Home.dart
+++ lib/presentation/home_screens/Home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fud/models/recipes/Recipe.dart';
 import 'package:fud/data/remote/ApiService.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatefulWidget {
 
@@ -33,14 +34,191 @@ class MyHomePage extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold
+      (
+
       appBar: AppBar(
         title: Text('Dio Test'),
       ),
       body: Center(
+        child: getrecipesUI()
+        ,
+      ),
 
+    );
+  }
+/*
+In Flutter, the FutureBuilder widget is used to asynchronously
+ build the UI based on the result of a Future.
+  It's a convenient way to handle asynchronous operations
+   and update your user interface when the data becomes available.
+FutureBuilder is a widget that takes two key parameters:
+future: This parameter expects a Future object.
+ It represents the asynchronous operation whose result you want to use to build the UI.
+  In your case, you are calling ApiService().getRecipes() to get a Future that will fetch recipe data.
+builder: This parameter is a callback function that takes two arguments, context and snapshot.
+ It is called when the Future completes, and its result is available.
+ The purpose of this callback is to build the UI based on the state of the Future.
+  It's where you define how your UI should look based on whether the Future is still loading,
+   has completed successfully, or encountered an error.
+*/
+  Column getrecipesUI() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // Widget for displaying recipes goes here
+        Expanded(
+          child: FutureBuilder<Recipe>(
+            future: ApiService().getrecipes(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                // Show a loading indicator while fetching data
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (snapshot.hasError) {
+                // Handle error state
+                return Center(
+                  child: Text('Error: ${snapshot.error}'),
+                );
+              } else if (!snapshot.hasData) {
+                // Handle the case when no data is available
+                return Center(
+                  child: Text('No recipe data available.'),
+                );
+              } else {
+                // Data is available, display the recipe content
+                Recipe? recipe = snapshot.data;
+                return RecipeWidget(recipe as AsyncSnapshot<Recipe>); // Replace 'RecipeWidget' with your widget to display the recipe.
+              }
+            },
+          ),
         ),
+      ],
+    );
 
+  }
+  SizedBox RecipeWidget(AsyncSnapshot<Recipe> snapshot)
+  {
+    return SizedBox(
+      child: ListView.builder(
+        scrollDirection: Axis.vertical ,
+
+          itemBuilder: (context,index){
+            return SizedBox(
+              height: 600,
+              child: Column(
+                children: [
+                  Container(
+                    height: 40,
+                    width: double.infinity,
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      // Vertically center the icon and text
+                      children: [
+                        const Icon(
+                          Icons.account_circle,
+                          // Replace this with the icon you want to use for the profile
+                          size: 24,
+                          color: Colors.black,
+                        ),
+                        const SizedBox(width: 8),
+                        // Add some spacing between the icon and the text
+                        Text(
+                          snapshot.data!.recipes[index].author,
+                          textAlign: TextAlign.start,
+                          style: GoogleFonts.poppins(
+                            fontSize: 16,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Card(
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Stack(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.network(
+                            snapshot.data!.recipes[index].image,
+                            height: 540,
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Positioned(
+                            bottom: 8,
+                            left: 8,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.black.withOpacity(0.5),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: IconButton(
+                                icon: Icon(
+                                  Icons.favorite_border,
+                                  size: 24,
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    // Toggle the 'liked' state of the photo
+                                   // snapshot.data!.photos[index].liked =
+                                    //!snapshot.data!.photos[index].liked;
+                                  });
+                                },
+                              ),
+                            )),
+
+                        //download section
+                        // Positioned(
+                        //     bottom: 8,
+                        //     right: 8,
+                        //     child: Container(
+                        //       decoration: BoxDecoration(
+                        //         color: Colors.black.withOpacity(0.5),
+                        //         borderRadius: BorderRadius.circular(10),
+                        //       ),
+                        //       child: IconButton(
+                        //         icon: isDownloading
+                        //             ? const Icon(
+                        //           Icons.download,
+                        //           color: Colors.white,
+                        //           size: 24,
+                        //         )
+                        //             : const Icon(
+                        //           Icons.downloading,
+                        //           color: Colors.white,
+                        //           size: 24,
+                        //         ),
+                        //         onPressed: () {
+                        //           isDownloading = true;
+                        //           Icons.downloading;
+                        //           String url =
+                        //               snapshot.data!.photos[index].src.original;
+                        //           String fileName =
+                        //           snapshot.data!.photos[index].id.toString();
+                        //           showDialog(
+                        //               context: context,
+                        //               builder: (context) =>
+                        //                   DownloadingDialog(url, fileName));
+                        //         },
+                        //       ),
+                        //     ))
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }
+
+      ),
     );
   }
 
