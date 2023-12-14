0e46bb547717c744e86086c6285964c1fc8ffe08
Rohit-554 <rohitkumarccc68@gmail.com>
2023-12-14 20:28:05 +0530

    Scrolling resolved

--- lib/presentation/routes/AppRouter.gr.dart
+++ lib/presentation/routes/AppRouter.gr.dart
@@ -8,27 +8,34 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i2;
+import 'package:auto_route/auto_route.dart' as _i3;
 import 'package:fud/presentation/auth_screens/LoginScreen.dart' as _i1;
+import 'package:fud/presentation/auth_screens/SignupScreen.dart' as _i2;
 
-abstract class $AppRouter extends _i2.RootStackRouter {
+abstract class $AppRouter extends _i3.RootStackRouter {
   $AppRouter({super.navigatorKey});
 
   @override
-  final Map<String, _i2.PageFactory> pagesMap = {
+  final Map<String, _i3.PageFactory> pagesMap = {
     LoginRoute.name: (routeData) {
-      return _i2.AutoRoutePage<dynamic>(
+      return _i3.AutoRoutePage<dynamic>(
         routeData: routeData,
         child: _i1.LoginScreen(),
       );
-    }
+    },
+    SignupRoute.name: (routeData) {
+      return _i3.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: _i2.SignupScreen(),
+      );
+    },
   };
 }
 
 /// generated route for
 /// [_i1.LoginScreen]
-class LoginRoute extends _i2.PageRouteInfo<void> {
-  const LoginRoute({List<_i2.PageRouteInfo>? children})
+class LoginRoute extends _i3.PageRouteInfo<void> {
+  const LoginRoute({List<_i3.PageRouteInfo>? children})
       : super(
           LoginRoute.name,
           initialChildren: children,
@@ -36,5 +43,19 @@ class LoginRoute extends _i2.PageRouteInfo<void> {
 
   static const String name = 'LoginRoute';
 
-  static const _i2.PageInfo<void> page = _i2.PageInfo<void>(name);
+  static const _i3.PageInfo<void> page = _i3.PageInfo<void>(name);
+}
+
+/// generated route for
+/// [_i2.SignupScreen]
+class SignupRoute extends _i3.PageRouteInfo<void> {
+  const SignupRoute({List<_i3.PageRouteInfo>? children})
+      : super(
+          SignupRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'SignupRoute';
+
+  static const _i3.PageInfo<void> page = _i3.PageInfo<void>(name);
 }
