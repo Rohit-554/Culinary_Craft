4a7e8498b7a2f226941e4f49ea8e33fc90789d4c
Rohit-554 <rohitkumarccc68@gmail.com>
2024-01-07 14:22:37 +0530

    recipe details screen

--- lib/presentation/home_screens/MyRecipeDetail.dart
+++ lib/presentation/home_screens/MyRecipeDetail.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../../models/meals/MealType.dart';
 
@@ -15,67 +16,149 @@ class MyRecipeDetail extends StatefulWidget {
   _MyRecipeDetailState createState() => _MyRecipeDetailState();
 }
 
-class _MyRecipeDetailState extends State<MyRecipeDetail> {  @override
+class _MyRecipeDetailState extends State<MyRecipeDetail> {
+  final ScrollController _scrollController = ScrollController();
+  bool isExpanded = true;
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+  void _onScroll() {
+  bool isExpanded = _scrollController.hasClients &&
+  _scrollController.offset > (400 - kToolbarHeight);
+    setState(() {
+      this.isExpanded = isExpanded;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-  return Scaffold(
-  appBar: AppBar(
-    title: const Text(''),
-    backgroundColor: Colors.white,
-    scrolledUnderElevation: 0.0,
-    automaticallyImplyLeading: true,
-    actions: [
-      IconButton(
-        onPressed: () {},
-        icon: Icon(Icons.favorite_outline_sharp),
-      ),
-    ],
-
-
-    ),
-    body: Container(
-      height: double.infinity,
-      width: double.infinity,
-      child: Column(
-        children: [
-          Expanded(
-            child:
-                  Column(
-                    children: [
-                      AspectRatio(
-                        aspectRatio: 16 / 9,
-                        child: Image.network(
-                          widget.snapshot.data!.meals![widget.index].strMealThumb!,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      Expanded(
-                          child: Card(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                            color: Colors.black,
-                            child: Expanded(
-                              child: ListView(
-                                shrinkWrap: true,
-                                physics: NeverScrollableScrollPhysics(),
-                                children: [
-
-                                ],
-                              ),
-                            ),
-                          )
-                      ),
-
-                    ],
-                  ),
+    return Scaffold(
+      /*appBar: AppBar(
+        title: const Text(''),
+        backgroundColor: Colors.white,
+        scrolledUnderElevation: 0.0,
+        automaticallyImplyLeading: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.favorite_outline_sharp),
           ),
         ],
       ),
-    ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          children: [
+            Expanded(
+              child: Column(
+                children: [
+                  AspectRatio(
+                    aspectRatio: 16 / 9,
+                    child: Image.network(
+                      widget.snapshot.data!.meals![widget.index].strMealThumb!,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Expanded(
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      color: Colors.black,
+                      child: Expanded(
+                        child: ListView(
+                          shrinkWrap: true,
+                          physics: NeverScrollableScrollPhysics(),
+                          children: [
+                            // Your ListView items go here
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),*/
+      body: CustomScrollView(
+        controller: _scrollController,
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 400,
+            pinned: true,
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              color: Colors.white,
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.favorite_outline_sharp),
+              ),
+            ],
+            systemOverlayStyle: const SystemUiOverlayStyle(
+              statusBarColor: Colors.white,
+              statusBarIconBrightness: Brightness.dark,
+            ),
+            stretch: true,
+            backgroundColor: Colors.blue,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(
+                widget.snapshot.data!.meals![widget.index].strMeal!,
+                style: TextStyle(
+                  color: isExpanded ? Colors.white : Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              collapseMode: CollapseMode.pin,
+              centerTitle: false,
+              expandedTitleScale: 1,
+              background: Image.network(
+                widget.snapshot.data!.meals![widget.index].strMealThumb!,
+                fit: BoxFit.cover,
+              ),
 
-
-
-
-  );
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Container(
+                  height: 1000,
+                  color: Colors.white,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          widget.snapshot.data!.meals![widget.index].strMeal!,
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+
