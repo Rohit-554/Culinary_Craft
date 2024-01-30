6dd9b9a9da4a039357f5d7ab872497c877350619
Saumya-28 <saumya18921@gmail.com>
2024-01-30 23:18:24 +0530

    add screen ui

--- lib/presentation/home_screens/AddScreen.dart
+++ lib/presentation/home_screens/AddScreen.dart
@@ -12,11 +12,35 @@ class _AddState extends State<AddScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      appBar: AppBar(),
+      body:
+
+      Column(
         children: [
-          
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: Card(
+              elevation: 2,
+              child: Container(
+                height: MediaQuery.of(context).size.height*0.4,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.grey[200],
+                ),
+                child: Center(
+                  child: Icon(
+                    Icons.add_photo_alternate_outlined,
+                    
+                    size: 100, // Adjust the size as needed
+                    color: Colors.black, // Adjust the color as needed
+                  ),
+                ),
+              ),
+            ),
+          ),
         ],
-      ),
+      )
+
     );
   }
 }
