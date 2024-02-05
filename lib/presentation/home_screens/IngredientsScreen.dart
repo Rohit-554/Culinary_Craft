f880048ef1caca37e8a33ce6ee099760364dcd28
Rohit-554 <rohitkumarccc68@gmail.com>
2024-02-05 23:28:43 +0530

    edit text

--- lib/presentation/home_screens/IngredientsScreen.dart
+++ lib/presentation/home_screens/IngredientsScreen.dart
@@ -17,39 +17,57 @@ class _IngredientsState extends State<IngredientsScreen> {
           padding: const EdgeInsets.all(12.0),
           child: Column(
             children: [
-
-
-                 Padding(
-                   padding: const EdgeInsets.only(left: 4,bottom: 8),
-                   child: Container(
-                     alignment: Alignment.topLeft,
-                     child: Text('Tags',
-
-                        style: TextStyle(fontWeight: FontWeight.bold)
-                                     ),
-                   ),
-                 ),
-
+              Padding(
+                padding: const EdgeInsets.only(left: 4, bottom: 8),
+                child: Container(
+                  alignment: Alignment.topLeft,
+                  child: const Text(
+                    'Tags',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
               Row(
                 children: [
-
-                  Container(
-                    width: MediaQuery.of(context).size.width*0.6,
-                    height: MediaQuery.of(context).size.height*0.04,
-                    decoration :BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: CupertinoColors.systemGrey5
+                  Expanded(
+                    flex: 7,
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.04,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: CupertinoColors.systemGrey5,
+                      ),
+                      child: const TextField(),
                     ),
-                    child: TextField(),
-
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.04,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.orangeAccent
+                      ),
+                      // Add your button widget here
+                      child: OutlinedButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Add',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
                 ],
-              )
+              ),
             ],
           ),
-        ) ,
-
+        ),
       ),
+
     );
   }
 }
