4cb8f6de9cabd6484c4135f44880b5a98804f838
Saumya-28 <saumya18921@gmail.com>
2024-02-06 00:33:25 +0530

    ingredients screen ui

--- lib/presentation/home_screens/IngredientsScreen.dart
+++ lib/presentation/home_screens/IngredientsScreen.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fud/colors/Colors.dart';
 @RoutePage()
 class IngredientsScreen extends StatefulWidget{
   @override
@@ -61,7 +62,154 @@ class _IngredientsState extends State<IngredientsScreen> {
                       ),
                     ),
                   ),
+
                 ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 4,top:12),
+                child: Container(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'Suggestions',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.04,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.deepOrangeAccent,
+
+
+                      ),
+
+                      child: OutlinedButton(
+                        onPressed: () {},
+                        style: OutlinedButton.styleFrom(
+                          side: BorderSide(width: 5.0, color: Colors.transparent),
+                        ),
+                        child: const Text(
+                          'Vegan',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.04,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.deepOrangeAccent,
+                      ),
+
+                      child: OutlinedButton(
+                        onPressed: () {},
+                        style: OutlinedButton.styleFrom(
+                          side: BorderSide(width: 5.0, color: Colors.transparent),
+                        ),
+                        child: const Text(
+                          'Dessert',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.04,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.deepOrangeAccent,
+                      ),
+
+                      child: OutlinedButton(
+                        onPressed: () {},
+                        style: OutlinedButton.styleFrom(
+                          side: BorderSide(width: 5.0, color: Colors.transparent),
+                        ),
+                        child: const Text(
+                          'Vegetarian',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.04,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.deepOrangeAccent,
+                      ),
+
+                      child: OutlinedButton(
+                        onPressed: () {},
+                        style: OutlinedButton.styleFrom(
+                          side: BorderSide(width: 5.0, color: Colors.transparent),
+                        ),
+                        child: const Text(
+                          'Meat',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.04,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.deepOrangeAccent,
+                      ),
+
+                      child: OutlinedButton(
+                        onPressed: () {},
+                        style: OutlinedButton.styleFrom(
+                          side: BorderSide(width: 5.0, color: Colors.transparent),
+                        ),
+                        child: const Text(
+                          'Seafood',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 2,top: 12),
+                child: Container(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'Ingredients',
+                        style: TextStyle(fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                ),
               ),
             ],
           ),
